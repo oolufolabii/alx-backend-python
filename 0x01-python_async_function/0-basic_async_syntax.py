67c3dd2ea39807473655ccd7fba9c724b8ee01d3
oolufolabii <otitoola.olufolabi@gmail.com>
2023-02-06 17:05:01 +0100

    Async Task 1

---
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""A python module on basics of async
+    """
+import asyncio
+import random
+
+
+async def basic_async(max_delay: int = 10) -> float:
+    '''Waits for a random number of seconds.
+    '''
+    wait_time = random.random() * max_delay
+    await asyncio.sleep(wait_time)
+    return wait_time
