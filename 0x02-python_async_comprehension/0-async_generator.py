a52d68f619dcc91535d837075a6974a32e3544a2
oolufolabii <otitoola.olufolabi@gmail.com>
2023-02-07 17:01:01 +0100

    Async Generator

---
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""A python module for a coroutine
+	"""
+
+import asyncio
+import random
+from typing import Generator
+
+
+async def async_generator() -> Generator[float, None, None]:
+	"""Generating a sequence of 10 random numbers
+	"""
+	for num in range(10):
+		await asyncio.sleep(1)
+		yield random.random() * 10
