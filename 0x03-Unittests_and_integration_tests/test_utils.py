45207eecc9776face5660c8f2bd364d4832ed843
oolufolabii <otitoola.olufolabi@gmail.com>
2023-02-26 08:08:16 +0100

    Integration and Unit Tests

---
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+"""Python module to test the utils program"""
+
+import unittest
+from typing import Dict, Tuple, Union
+from unittest.mock import patch, Mock
+from parameterized import parameterized
+
+from utils import (
+    access_nested_map,
+    get_json,
+    memoize,
+)
+
+
+class TestAccessNestedMap(unittest.TestCase):
+    """Class to test the utils.access_nested_map
+    from utils.py"""
+    @parameterized.expand([
+        ({"a": 1}, ("a",), 1),
+        ({"a": {"b": 2}}, ("a",), {"b": 2}),
+        ({"a": {"b": 2}}, ("a", "b"), 2),
+    ])
+    def test_access_nested_map(
+            self,
+            nested_map: Dict,
+            path: Tuple[str],
+            expected: Union[Dict, int],
+    ) -> None:
+        """Testing the access_nested_map output."""
+        self.assertEqual(access_nested_map(nested_map, path), expected)
